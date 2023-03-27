b281baea034e42531a5d27339817b87c42b71909
vichujesi <129038648+vichujesi@users.noreply.github.com>
2023-03-27 12:50:54 +0530

    Update lab.py

--- lab.py
+++ lab.py
@@ -5,6 +5,8 @@ exam_data={'name':['Anastasia','dima','katherine','james','emily','micheal','mat
            'attempts':[1,3,2,3,2,3,1,1,2,1],
            'qualify':['yes','no','yes','no','no','yes','yes','no','no','yes']}
 labels=['a','b','c','d','e','f','g','h','i','j']
+
+
 df = pd.DataFrame(exam_data, index=labels)
 x=pd.DataFrame(exam_data)
 print(x)
